337b180c8429b38f7e7429d7b62020d7868a7175
masonkarsevar <masonkarsevar@gmail.com>
2019-11-17 09:59:56 -0800

    added monthly functionality to the backend server

--- index.js
+++ index.js
@@ -32,7 +32,10 @@ cron.schedule(`*/1 * * * *`, function() {
             for (let i = 0; i < results.data.data.length; i++) {
                 // Logic that tells the scheduler whether the message should be sent monthly
                 // or weekly. Empty month string means that the message should be sent weekly.
-                if (results.data.data[i].month === '') {
+                if (
+                    results.data.data[i].month === '' &&
+                    results.data.data[i].dom === ''
+                ) {
                     // converts the values from the scheduledMessages table to Tuesday,6:06pm
                     // so that we can use moment to check if the message should be sent.
                     if (
@@ -64,7 +67,40 @@ cron.schedule(`*/1 * * * *`, function() {
                             )
                             .catch(err => console.log(error, err));
                     } else {
-                        console.log('no messages scheduled at this time.');
+                        console.log(
+                            'no messages scheduled at this time for daily'
+                        );
+                    }
+                } else if (results.data.data[i].month === '') {
+                    // Monthly messages logic.
+                    if (
+                        `${results.data.data[i].dom} ${
+                            results.data.data[i].hour
+                        }:${results.data.data[i].min}${results.data.data[
+                            i
+                        ].ampm.toLowerCase()}` ===
+                        `${moment().format('DD')} ${moment().format('h:mma')}`
+                    ) {
+                        const cleanedNumber = results.data.data[
+                            i
+                        ].userPhone.replace(
+                            /(\d{3})(\d{3})(\d{4})/,
+                            '($1) $2-$3'
+                        );
+                        client.messages
+                            .create({
+                                body: results.data.data[i].msg,
+                                from: `${process.env.TWILIO_NUMBER}`,
+                                to: `+1${cleanedNumber}`
+                            })
+                            .then(message =>
+                                console.log(`message sent ${message.sid}.`)
+                            )
+                            .catch(err => console.log(error, err));
+                    } else {
+                        console.log(
+                            'no messages scheduled at this time for monthly'
+                        );
                     }
                 } else {
                     // logic for the messages that are scheduled to be sent monthly.
@@ -99,7 +135,9 @@ cron.schedule(`*/1 * * * *`, function() {
                             )
                             .catch(err => console.log(error, err));
                     } else {
-                        console.log('no messages scheduled at this time.');
+                        console.log(
+                            'no messages scheduled at this time for yearly'
+                        );
                     }
                 }
             }
@@ -107,6 +145,7 @@ cron.schedule(`*/1 * * * *`, function() {
         .catch(err => {
             console.log(err);
         });
+    // console.log('moment date of the month test', moment().format('DD'));
 });
 
 const server = express();
